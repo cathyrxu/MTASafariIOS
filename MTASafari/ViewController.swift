81b2c57256eaa43bec8759c0d2b61f50f2c543a4
Cath Xu <cathxu@CathdeAir.lan>
2025-09-11 23:35:29 -0400

    {initial_commit_message}

---
+++ MTASafari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTASafari
+//
+//  Created by Cath Xu on 9/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
